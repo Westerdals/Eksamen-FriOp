9b49299e73c5830492adb0e741b4ae6689a8b8a3
OpKris <kristofferopdahl@mail.com>
2019-10-30 16:32:56 +0100

    added a simple test

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RunTest {
+
+    @Test
+    void shouldReturnFour(){
+        assertEquals(4, 2+2);
+    }
+}
